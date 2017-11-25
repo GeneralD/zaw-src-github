6a2d09ee1883f023fb95a9cb24beedf05cccd206
GeneralD <yumejustice@gmail.com>
2017-11-25 18:57:13 +0900

    [from now] 2017/11/25 18:57:13
    diff --git a/src/variables.zsh b/src/variables.zsh
    index 6541914..21359d6 100755
    --- a/src/variables.zsh
    +++ b/src/variables.zsh
    @@ -1,4 +1,4 @@
     #!/usr/bin/env zsh
    
    -ZAW_GITHUB_USER=${ZAW_GITHUB_USER:-`\cat .config/hub 2>/dev/null | \grep user | \awk '{ print $NF }'`}
    -ZAW_GITHUB_TOKEN=${ZAW_GITHUB_TOKEN:-`\cat .config/hub 2>/dev/null | \grep oauth_token | \awk '{ print $NF }'`}
    +ZAW_GITHUB_USER=${ZAW_GITHUB_USER:-`\cat ~/.config/hub 2>/dev/null | \grep user | \awk '{ print $NF }'`}
    +ZAW_GITHUB_TOKEN=${ZAW_GITHUB_TOKEN:-`\cat ~/.config/hub 2>/dev/null | \grep oauth_token | \awk '{ print $NF }'`}

--- src/variables.zsh
+++ src/variables.zsh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
-ZAW_GITHUB_USER=${ZAW_GITHUB_USER:-`\cat .config/hub 2>/dev/null | \grep user | \awk '{ print $NF }'`}
-ZAW_GITHUB_TOKEN=${ZAW_GITHUB_TOKEN:-`\cat .config/hub 2>/dev/null | \grep oauth_token | \awk '{ print $NF }'`}
+ZAW_GITHUB_USER=${ZAW_GITHUB_USER:-`\cat ~/.config/hub 2>/dev/null | \grep user | \awk '{ print $NF }'`}
+ZAW_GITHUB_TOKEN=${ZAW_GITHUB_TOKEN:-`\cat ~/.config/hub 2>/dev/null | \grep oauth_token | \awk '{ print $NF }'`}
